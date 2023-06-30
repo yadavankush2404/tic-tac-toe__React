8d0b9af66c60effb991e9951374c769bcaed76fa
ANKUSH YADAV <ankushyadav@ANKUSHs-MacBook-Air.local>
2023-06-30 12:41:54 +0530

    Responsiviness add

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Task from './components/task/Task'
 
--- src/components/task/Cell.js
+++ src/components/task/Cell.js
@@ -1,12 +1,7 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './Cell.css';
 
 const Cell = (props) =>{
-
-    const [mark ,setMark] = useState(props.vaule);
-
-    
-
     // const display = () =>{
     //     console.log("click");
     //     setMark('X');
@@ -16,7 +11,7 @@ const Cell = (props) =>{
     return(
         console.log("bo"),
         console.log(props.bo),
-        <button className={props.bo == 'X' ? "cell x" : "cell o" } onClick={props.onClick}  >{props.vaule}</button>
+        <button className={props.bo === 'X' ? "cell x" : "cell o" } onClick={props.onClick}  >{props.vaule}</button>
     )
 }
 
